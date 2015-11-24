860e537bb62f63b821241b60b8a62346e7b6bcb9
KahWee Teng <kahwee@kteng-mbp.tremor.local>
2015-11-24 13:50:00 -0800

    Add eslint

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # vpaid-ad
 
+[![bitHound Score](https://www.bithound.io/github/kahwee/vpaid-ad/badges/score.svg)](https://www.bithound.io/github/kahwee/vpaid-ad)
+
 This is a reference implementation of VPAID ad. Feel free to extend this to suit your needs.
 
 Resources:
--- src/handler/click-thru.js
+++ src/handler/click-thru.js
@@ -1,9 +1,9 @@
 export default function () {
   const clickThru = this._parameters.clickThru || {
-      url: 'http://www.example.com',
-      trackID: 123,
-      playerHandles: false
-    }
+    url: 'http://www.example.com',
+    trackID: 123,
+    playerHandles: false
+  }
 
   $trigger.call(this, 'AdClickThru', [clickThru.url, clickThru.trackID, clickThru.playerHandles])
 
